b9f3e2d0bd5257defba5348e06200f63530c8731
Roberto Kennedy <roberto.kpp@gmail.com>
2026-02-06 12:37:11 -0500

    feat: initialize mundo-pet application with webpack setup
    - Added package.json with project metadata and dependencies.
    - Removed unused index.js and index.css files.
    - Created server.json for scheduling data.
    - Added main.js to serve as the entry point for the application.
    - Configured webpack with HtmlWebpackPlugin and CopyWebpackPlugin for asset management.

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//CSS 
+import "./styles/aside.css"
+import "./styles/dialog.css"
+import "./styles/form.css"
+import "./styles/global.css"
+import "./styles/utility.css"
+
+
+// scripts
+import "./scripts/modal.js"
